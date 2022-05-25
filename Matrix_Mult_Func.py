9e853ced1dbfcbac4cdbc4ec0352811f6fbe6d90
Elizabeth <liz@CORE2025.montclair.edu>
2022-05-25 16:33:44 -0400

    updated the function for mat_mult for any shape and size

--- Matrix_Mult_Func.py
+++ Matrix_Mult_Func.py
@@ -1,9 +1,37 @@
 import numpy as np
 
+def rows_and_cols(x):
+    """
+    This function takes as input one matrix and runs  an if-else
+    statement that computes the length of the necessary matrix for the
+    following definition function. If the matrix is a 2x1, it will print 
+    in the proper format. Otherwise, the function will print out the proper
+    format automatically.
+
+    INPUT:
+    -------
+    x : Numpy array of a matrix
+
+    RETURNS:
+    --------
+    The length of the Numpy array
+    """
+
+    y = np.shape(x)
+    z = len(y)
+    if z > 1:
+        return y
+    else:
+        return (y[0], 1)
+
+
 def mat_mult(aa, bb):
     """
-    This function takes as input two matrices of same shape,
-    and computes their product.
+    This function takes as input two matrices that may or may not be
+    of the same shape/size, and computes their product. If the arrays
+    are incomputible, an assertion error will pop up; otherwise, the
+    function will produce the product of the multiplication of the 
+    two matrices.
 
     INPUT:
     ------
@@ -15,59 +43,18 @@ def mat_mult(aa, bb):
     -------
     A numpy array
     """
-    cc = np.zeros(np.shape(aa))
-    nn = len(cc)
-    for ii in range(0, nn):
-        for jj in range(0, nn):
-            cc[ii, jj] = np.sum(aa[ii]*bb[:,jj])
+dim_aa = rows_and_cols(aa)
+    dim_bb = rows_and_cols(bb)
+    assert dim_aa[1] == dim_bb[0], print("Matrices cannot be multiplied")
+    
+    cc = np.zeros((dim_aa[0], dim_bb[1]))
+    if dim_bb[1] == 1:
+        cc = cc.flatten()
+        for ii in range(0, dim_aa[0]):
+            cc[ii] = np.sum(aa[ii]*bb)
+    else:
+        for ii in range(0, dim_aa[0]):
+            for jj in range(0, dim_bb[1]):
+                cc[ii, jj] = np.sum(aa[ii]*bb[:,jj])
     return cc
-
-def mat_mult2(a, b):
-    """
-    This function takes as input two matrices of same shape,
-    and computes their product.
-
-    INPUT:
-    ------
-    A : Numpy array
-    B : Numpy array
-
-    RETURNS:
-    --------
-    The product of the two numpy arrays
-    """
-    a_row_cnt, b_col_cnt = np.shape(a)
-    a_col_cnt = a_row_cnt
-    c = np.zeros((a_row_cnt, b_col_cnt))
-
-    for c_row_ii in range(0, a_row_cnt):
-        for c_col_ii in range(0, b_col_cnt):
-            for ii in range(0, a_col_cnt):
-                c[c_row_ii, c_col_ii] += a[c_row_ii, ii] * b[ii, c_col_ii]
-
-    return c
-
-
-def mat_sq_col1(ff,gg):
-    """
-    This function takes as input two matrices of different shapes,
-    and computes their product.
-
-    INPUT:
-    ------
-    ff : Numpy array of first matrix
-    gg : Numpy array of second matrix
-
-    RETURNS:
-    --------
-    The product of the two numpy arrays
-    """
-
-    hh = np.zeros((min(np.shape(ff)),min(np.shape(gg))))
-    pp = len(hh)
-    for ii in range(0, pp):
-        for jj in range(0, pp):
-            gg == gg.T
-            hh[ii, jj] = np.sum(ff[ii]*gg[:,jj])
-    return hh
 
